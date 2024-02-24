1891b069b915fc8fd7ef480a1e31f55412d53217
rory668 <treblecc121@outlook.com>
2024-02-24 00:46:34 +0000

    Initial commit

---
+++ src/com/studio/shade/SystemUIFactory.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.studio.shade;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.internal.widget.LockPatternUtils;
+import com.android.keyguard.ViewMediatorCallback;
+import com.studio.shade.statusbar.ScrimView;
+import com.studio.shade.statusbar.phone.KeyguardBouncer;
+import com.studio.shade.statusbar.phone.NotificationIconAreaController;
+import com.studio.shade.statusbar.phone.PhoneStatusBar;
+import com.studio.shade.statusbar.phone.QSTileHost;
+import com.studio.shade.statusbar.phone.ScrimController;
+import com.studio.shade.statusbar.phone.StatusBarIconController;
+import com.studio.shade.statusbar.phone.StatusBarKeyguardViewManager;
+import com.studio.shade.statusbar.phone.StatusBarWindowManager;
+import com.studio.shade.statusbar.policy.BatteryController;
+import com.studio.shade.statusbar.policy.BluetoothController;
+import com.studio.shade.statusbar.policy.CastController;
+import com.studio.shade.statusbar.policy.FlashlightController;
+import com.studio.shade.statusbar.policy.HotspotController;
+import com.studio.shade.statusbar.policy.KeyguardMonitor;
+import com.studio.shade.statusbar.policy.LocationController;
+import com.studio.shade.statusbar.policy.NetworkController;
+import com.studio.shade.statusbar.policy.NextAlarmController;
+import com.studio.shade.statusbar.policy.RotationLockController;
+import com.studio.shade.statusbar.policy.SecurityController;
+import com.studio.shade.statusbar.policy.UserInfoController;
+import com.studio.shade.statusbar.policy.UserSwitcherController;
+import com.studio.shade.statusbar.policy.ZenModeController;
+
+/**
+ * Class factory to provide customizable SystemUI components.
+ */
+public class SystemUIFactory {
+    private static final String TAG = "SystemUIFactory";
+
+    static SystemUIFactory mFactory;
+
+    public static SystemUIFactory getInstance() {
+        return mFactory;
+    }
+
+    public static void createFromConfig(Context context) {
+        final String clsName = context.getString(R.string.config_systemUIFactoryComponent);
+        if (clsName == null || clsName.length() == 0) {
+            throw new RuntimeException("No SystemUIFactory component configured");
+        }
+
+        try {
+            Class<?> cls = null;
+            cls = context.getClassLoader().loadClass(clsName);
+            mFactory = (SystemUIFactory) cls.newInstance();
+        } catch (Throwable t) {
+            Log.w(TAG, "Error creating SystemUIFactory component: " + clsName, t);
+            throw new RuntimeException(t);
+        }
+    }
+
+    public SystemUIFactory() {}
+
+    public StatusBarKeyguardViewManager createStatusBarKeyguardViewManager(Context context,
+            ViewMediatorCallback viewMediatorCallback, LockPatternUtils lockPatternUtils) {
+        return new StatusBarKeyguardViewManager(context, viewMediatorCallback, lockPatternUtils);
+    }
+
+    public KeyguardBouncer createKeyguardBouncer(Context context, ViewMediatorCallback callback,
+            LockPatternUtils lockPatternUtils, StatusBarWindowManager windowManager,
+            ViewGroup container) {
+        return new KeyguardBouncer(context, callback, lockPatternUtils, windowManager, container);
+    }
+
+    public ScrimController createScrimController(ScrimView scrimBehind, ScrimView scrimInFront,
+            View headsUpScrim) {
+        return new ScrimController(scrimBehind, scrimInFront, headsUpScrim);
+    }
+
+    public NotificationIconAreaController createNotificationIconAreaController(Context context,
+            PhoneStatusBar phoneStatusBar) {
+        return new NotificationIconAreaController(context, phoneStatusBar);
+    }
+
+    public QSTileHost createQSTileHost(Context context, PhoneStatusBar statusBar,
+            BluetoothController bluetooth, LocationController location,
+            RotationLockController rotation, NetworkController network,
+            ZenModeController zen, HotspotController hotspot,
+            CastController cast, FlashlightController flashlight,
+            UserSwitcherController userSwitcher, UserInfoController userInfo,
+            KeyguardMonitor keyguard, SecurityController security,
+            BatteryController battery, StatusBarIconController iconController,
+            NextAlarmController nextAlarmController) {
+        return new QSTileHost(context, statusBar, bluetooth, location, rotation, network, zen,
+                hotspot, cast, flashlight, userSwitcher, userInfo, keyguard, security, battery,
+                iconController, nextAlarmController);
+    }
+
+    public <T> T createInstance(Class<T> classType) {
+        return null;
+    }
+}
