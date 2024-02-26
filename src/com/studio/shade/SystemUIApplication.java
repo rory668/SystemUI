1505d7fa6a247c30311967dc42a833ddd47d2ce0
Rory <76168232+rory668@users.noreply.github.com>
2024-02-26 16:07:18 +0530

    Update SystemUIApplication.java

--- src/com/studio/shade/SystemUIApplication.java
+++ src/com/studio/shade/SystemUIApplication.java
@@ -45,7 +45,6 @@ public class SystemUIApplication extends Application {
      */
     private final Class<?>[] SERVICES = new Class[] {
             com.studio.shade.tuner.TunerService.class,
-            com.studio.shade.keyguard.KeyguardViewMediator.class,
             com.studio.shade.volume.VolumeUI.class,
             Divider.class,
             com.studio.shade.statusbar.SystemBars.class,
